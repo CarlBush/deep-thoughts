af37f18c6bb7f553b617f0a60f4ecaab2c945859
Carl Bush <CarlBushs13@gmail.com>
2022-06-18 15:06:00 -0700

    thought query created with username parameter

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,12 @@
+const { User, Thought } = require("../models");
+
 const resolvers = {
     Query: {
-        helloWorld: () => {
-            return "Hello world!";
+        thoughts: async (parent, { username }) => {
+            //if username exists use that if not return empty object
+            const params = username ? { username } : {};
+            //returning data in descending order based on sort createdAt
+            return Thought.find(params).sort({ createdAt: -1 });
         }
     }
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,9 +1,20 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
+
+type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+}
+
 type Query {
-    helloWorld: String
+    thoughts(username: String): [Thought]
     }
 `;
+
+
 
 module.exports = typeDefs;
