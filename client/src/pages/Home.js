843e2e50e668b968976b521366bf487858d785a0
Carl Bush <CarlBushs13@gmail.com>
2022-06-19 11:11:02 -0700

    login friends list not working

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
-import { QUERY_THOUGHTS } from '../utils/queries';
+import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
 import ThoughtList from '../components/ThoughtList';
+import Auth from '../utils/auth';
+import FriendList from "../components/FriendList";
 
 const Home = () => {
   //use useQuery hook to make query request
@@ -10,18 +12,39 @@ const Home = () => {
   that information is stored in the destructured data property.*/
 
   const { loading, data } = useQuery(QUERY_THOUGHTS);
+
+
+  // use object destructuring to extract `data` from the `useQuery` Hook's response and rename it `userData` to be more descriptive
+  const { data: userData } = useQuery(QUERY_ME_BASIC);
+
   //if data exists, store it in the thoughts constant we just created. If data is undefined, then save an empty array to the thoughts component.
   const thoughts = data?.thoughts || [];
-  console.log(thoughts);
+
+  //if logged in loggedIn variable will be true
+  const loggedIn = Auth.loggedIn();
   return (
     <main>
       <div className='flex-row justify-space-between'>
-        <div className='col-12 mb-3'>{loading ? (
-          <div> Loading </div>
-        ) : (
-          <ThoughtList thoughts={thoughts} title="Some Feed for Thought(s)..." />
-        )}
+
+        {/* If the user isn't logged in, it'll span the full width of the row. 
+        But if you the user is logged in, it'll only span eight columns, 
+        leaving space for a four-column <div> on the righthand side.*/}
+        <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <ThoughtList thoughts={thoughts} title="Some Feed for Thought(s)..." />
+          )}
         </div>
+        {loggedIn && userData ? (
+          <div className="col-12 col-lg-3 mb-3">
+            <FriendList
+              username={userData.me.username}
+              friendCount={userData.me.friendCount}
+              friends={userData.me.friends}
+            />
+          </div>
+        ) : null}
       </div>
     </main>
   );
