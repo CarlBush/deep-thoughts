2c2e5f70904f68b36e61a2f7ca8a6f6c05954362
Carl Bush <CarlBushs13@gmail.com>
2022-06-19 10:13:05 -0700

    logout function and nav update

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import Auth from "../../utils/auth";
 
 const Header = () => {
+
+  const logout = event => {
+    //event.preventDefault(), we're actually overriding the <a> element's default. instead, we execute the .logout() method, which will remove the token from localStorage
+    event.preventDefault();
+    Auth.logout();
+  };
+
   return (
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <Link to="/">
-        <h1>Deep Thoughts</h1>
+          <h1>Deep Thoughts</h1>
         </Link>
 
-        <nav className='text-center'>
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Signup</Link>
+        {/*
+        Auth.loggedIn()
+        If it returns true, and we're logged in, we want to display navigation items tailored to the user. 
+        If it returns false, we'll display the default items for logging in and signing up.*/}
+        <nav className="text-center">
+          {Auth.loggedIn() ? (
+            <>
+              <Link to="/profile">Me</Link>
+              <a href="/" onClick={logout}>Logout</a>
+            </>
+          ) : (
+            <>
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Signup</Link>
+            </>
+          )}
         </nav>
       </div>
     </header>
--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,7 +1,7 @@
 import decode from 'jwt-decode';
 
 class AuthService {
-    //retrieve data saved in the token
+    // retrieve data saved in token
     getProfile() {
         return decode(this.getToken());
     }
@@ -28,20 +28,26 @@ class AuthService {
         }
     }
 
+    // retrieve token from localStorage
+    getToken() {
+        // Retrieves the user token from localStorage
+        return localStorage.getItem('id_token');
+    }
+
     // set token to localStorage and reload page to homepage
     login(idToken) {
         // Saves user token to localStorage
-        localStorage.setItem("id_token", idToken);
+        localStorage.setItem('id_token', idToken);
 
-        window.location.assign("/");
+        window.location.assign('/');
     }
 
     // clear token from localStorage and force logout with reload
     logout() {
         // Clear user token and profile data from localStorage
-        localStorage.removeItem("id_token");
+        localStorage.removeItem('id_token');
         // this will reload the page and reset the state of the application
-        window.location.assign("/");
+        window.location.assign('/');
     }
 }
 
