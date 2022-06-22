50359be8c6eac0b884bb735e40ba5c523beb0415
Carl Bush <CarlBushs13@gmail.com>
2022-06-22 13:34:39 -0700

    profile.js

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -9,11 +9,13 @@ import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
 import Auth from '../utils/auth';
 
-import { useQuery } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import { ADD_FRIEND } from '../utils/mutations';
 
 const Profile = () => {
   const { username: userParam } = useParams();
+  const [addFriend] = useMutation(ADD_FRIEND);
 
   //Now if there's a value in userParam that we got from the URL bar, we'll use that value to run the QUERY_USER query.
   //If there's no value in userParam, like if we simply visit /profile as a logged-in user, we'll execute the QUERY_ME query instead.
@@ -31,8 +33,8 @@ const Profile = () => {
   if (loading) {
     return <div>Loading...</div>;
   }
-//there is no user data to display, we know that we aren't logged in or at another user's profile page. 
-//Instead of redirecting the user away, we simply inform them that they need to be logged in to see this page and they must log in or sign up to use it.
+  //there is no user data to display, we know that we aren't logged in or at another user's profile page. 
+  //Instead of redirecting the user away, we simply inform them that they need to be logged in to see this page and they must log in or sign up to use it.
   if (!user?.username) {
     return (
       <h4>
@@ -40,12 +42,24 @@ const Profile = () => {
       </h4>
     );
   }
+
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id }
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
           Viewing {userParam ? `${user.username}'s` : 'your'} profile.
         </h2>
+        <button className='btn ml-auto' onClick={handleClick}>Add Friend</button>
       </div>
 
       <div className="flex-row justify-space-between mb-3">
